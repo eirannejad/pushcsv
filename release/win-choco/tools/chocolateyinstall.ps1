63d0b108c3ee2d366ab42bfe54fa1641fcc76e51
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-04-01 12:42:00 -0700

    Released v1.1 on choco

--- release/win-choco/tools/chocolateyinstall.ps1
+++ release/win-choco/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/eirannejad/pushcsv/releases/download/v1.0/pushcsv.zip'
+$url        = 'https://github.com/eirannejad/pushcsv/releases/download/v1.1/pushcsv-win64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'pushcsv*'
-  checksum      = 'C6B929B666B0098B00D6DFF22A03E918A302580D6BE236AD516869790C775D90'
+  checksum      = 'A75A9E361A584582FC145181A793BC07F142D76C848B3C53CA9BE640A664B252'
   checksumType  = 'sha256'
 }
 
