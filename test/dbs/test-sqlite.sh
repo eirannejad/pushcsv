a174793d52ec4e6d95a4b0fc76bdd0f8b91c8cd1
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-08-15 20:26:58 -0700

    1.6 tests

--- test/dbs/test-sqlite.sh
+++ test/dbs/test-sqlite.sh
@@ -1,11 +1,11 @@
 #!
 echo "running pushcsv tests on sqlite3..."
 
-TABLENAME="sqlitetesttable"
-TESTDB=$TESTPATH/sqlitetestdb.db
+# TABLENAME="sqlitetesttable"
+# TESTDB=$TESTPATH/sqlitetestdb.db
 
-# export the db config and run tests
-$TESTCASES/test-all.sh "sqlite3:$TESTDB" $TABLENAME
+# # export the db config and run tests
+# $TESTCASES/test-all.sh "sqlite3:$TESTDB" $TABLENAME
 
-printf $PROGRESSPRINT "cleaning up..."
-# rm $TESTDB
+# printf $PROGRESSPRINT "cleaning up..."
+# # rm $TESTDB
